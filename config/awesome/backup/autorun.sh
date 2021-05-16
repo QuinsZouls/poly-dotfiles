f05667be421f440fca34bf8e2750757e04cf85b6
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2021-05-16 01:25:08 -0500

    Initial commit

---
+++ config/awesome/backup/autorun.sh
@@ -0,0 +1,1 @@
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal --output HDMI-1 --off --output HDMI-1-0 --mode 2560x1080 --pos 1280x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
---
+++ config/awesome/rc.lua
@@ -0,0 +1,79 @@
+-- ░█▀▀░█░█░█▀▄░█▀▄░█▀▀░█▀█░█░░
+-- ░▀▀█░█░█░█▀▄░█▀▄░█▀▀░█▀█░█░░
+-- ░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀▀▀
+-- Banner generated using `toilet -f pagga AwesomeWM"
+
+local gears = require('gears')
+local beautiful = require('beautiful')
+local awful = require('awful')
+require('awful.autofocus')
+
+-- ░█▀▀░█░█░█▀▀░█░░░█░░
+-- ░▀▀█░█▀█░█▀▀░█░░░█░░
+-- ░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀▀▀
+
+awful.util.shell = 'sh'
+
+-- ░▀█▀░█░█░█▀▀░█▄█░█▀▀
+-- ░░█░░█▀█░█▀▀░█░█░█▀▀
+-- ░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+beautiful.init(require('theme'))
+
+-- ░█░░░█▀█░█░█░█▀█░█░█░▀█▀
+-- ░█░░░█▀█░░█░░█░█░█░█░░█░
+-- ░▀▀▀░▀░▀░░▀░░▀▀▀░▀▀▀░░▀░
+
+require('layout')
+
+-- ░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀░█░█░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀
+-- ░█░░░█░█░█░█░█▀▀░░█░░█░█░█░█░█▀▄░█▀█░░█░░░█░░█░█░█░█░▀▀█
+-- ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀
+
+require('configuration.client')
+require('configuration.root')
+require('configuration.tags')
+root.keys(require('configuration.keys.global'))
+
+-- ░█▄█░█▀█░█▀▄░█░█░█░░░█▀▀░█▀▀
+-- ░█░█░█░█░█░█░█░█░█░░░█▀▀░▀▀█
+-- ░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀
+
+require('module.notifications')
+require('module.auto-start')
+require('module.exit-screen')
+require('module.quake-terminal')
+require('module.menu')
+require('module.titlebar')
+require('module.brightness-osd')
+require('module.volume-osd')
+require('module.lockscreen')
+require('module.dynamic-wallpaper')
+
+-- ░█░█░█▀█░█░░░█░░░█▀█░█▀█░█▀█░█▀▀░█▀▄
+-- ░█▄█░█▀█░█░░░█░░░█▀▀░█▀█░█▀▀░█▀▀░█▀▄
+-- ░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░░░▀░▀░▀░░░▀▀▀░▀░▀
+awful.spawn.with_shell("setxkbmap latam")
+awful.spawn.with_shell("xrandr --output eDP-1  --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --primary --mode 1920x1080 --rate 144 --pos 1920x1080 --rotate normal --output HDMI-1 --off --output HDMI-1-0 --mode 2560x1080 --pos 1280x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off")
+screen.connect_signal(
+	'request::wallpaper',
+	function(s)
+		-- If wallpaper is a function, call it with the screen
+		if beautiful.wallpaper then
+			if type(beautiful.wallpaper) == 'string' then
+
+				-- Check if beautiful.wallpaper is color/image
+				if beautiful.wallpaper:sub(1, #'#') == '#' then
+					-- If beautiful.wallpaper is color
+					gears.wallpaper.set(beautiful.wallpaper)
+
+				elseif beautiful.wallpaper:sub(1, #'/') == '/' then
+					-- If beautiful.wallpaper is path/image
+					gears.wallpaper.maximized(beautiful.wallpaper, s)
+				end
+			else
+				beautiful.wallpaper(s)
+			end
+		end
+	end
+)

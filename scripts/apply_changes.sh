814d3521044a6a0e80c86369165f8d605c5c2b54
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2021-05-20 00:38:20 -0500

    Add restart function

--- scripts/apply_changes.sh
+++ scripts/apply_changes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 yes | cp -TRv ../config/awesome/ ~/.config/awesome/
-echo "Changes applied, please restart awesome"
+echo "Changes applied, please restarting awesome..."
+awesome-client "awesome.restart()"
